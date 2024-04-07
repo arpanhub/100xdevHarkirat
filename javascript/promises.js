e1a358c89b2b5782493c083d101e716ea2acc2c4
Arpan Gupta <arpan.gupta@lpu.in>
2024-04-07 23:50:38 +0530

    commit

---
+++ javascript/promises.js
@@ -0,0 +1,48 @@
+//=>pending,resolved
+/* var d = new Promise(function(resolve){
+    resolve("helllo");
+    setTimeout(function(){
+        resolve("hello");
+    },1000) 
+}); */
+/* function callback(){
+    console.log(d);
+} */
+// console.log(d);
+/* d.then(function(){
+    console.log(d);
+}); */
+
+
+
+//----------------------------------------
+/* function ArpanAsyncFunction(){
+    let p=new Promise(function(resolve){
+        resolve("hi there");
+    });
+    return p;
+}
+function main(){
+    ArpanAsyncFunction().then(function(value){
+        console.log(value);
+    });
+}  
+main(); */
+
+
+//---------------------------------------------
+// Cleaner version
+function ArpanAsyncFunction(){
+    let p=new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("hi there");
+        },2000)
+    });
+    return p;
+}
+async function main(){
+    //no callbacks,no .then syntax
+   let value = await ArpanAsyncFunction();
+   console.log(value);
+}  
+main();
