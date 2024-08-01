36910aa56804b08fa6745e9a8757535b861a07f3
Arpan Gupta <arpan.gupta@lpu.in>
2024-08-02 02:09:43 +0530

    done assingments for week 3

--- assignments/week-3/03-mongo/routes/admin.js
+++ assignments/week-3/03-mongo/routes/admin.js
@@ -1,18 +1,67 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const {Admin, Course} = require('../db');   
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    await Admin.create({
+        username: username,
+        password: password
+    })
+    res.json({
+        "msg:" : "Admin created Successfully"
+    })
+
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+    const course_object =  await Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    })
+    res.json({
+        message:"Course added succedfully",
+        course_object: course_object._id
+    })
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware,async (req, res) => {
     // Implement fetching all courses logic
+    
+    const list_of_courses = await Course.find({}).exec();
+    if(list_of_courses.length === 0){
+        res.json({message:"add courses please"});
+    }else{
+        res.json({list_of_courses});
+    }
+
+
 });
 
 module.exports = router;
+// {
+//     {
+//         "list_of_courses": [
+//             {
+//                 "_id": "66abca1e7144da02c831774a",
+//                 "title": "harkirat-web3",
+//                 "description": "web3 + cohort 2",
+//                 "imageLink": "https//vjaoniebvirubo",
+//                 "price": 5000,
+//                 "__v": 0
+//             }
+//         ]
+//     }
+// }
--- assignments/week-3/03-mongo/routes/user.js
+++ assignments/week-3/03-mongo/routes/user.js
@@ -1,22 +1,63 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',async (req, res) => {
     // Implement user signup logic
+    const {username,password} = req.body;
+    const flag = await User.findOne({username})
+    if(!flag){
+        User.create({
+            username:username,
+            password:password
+        })
+        .then(function(){
+            res.json({msg:"user created succesfully"});
+        })
+    }else{
+        res.json({msg:"user already exist with this username"})
+    }
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async(req, res) => {
     // Implement listing all courses logic
+    const purchased_Courses = await Course.find({}).exec();
+    res.json({
+        List_Courses:purchased_Courses
+    })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async(req, res) => {
     // Implement course purchase logic
+    const courseID = req.params.courseId;
+    const username = req.headers.username;
+    await User.updateOne({
+        username : username
+    },{
+        "$push":{
+            purchasedCourses:courseID
+        }
+    })
+    res.json({
+        msg:"purchased succesfully"
+    })
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware,async (req, res) => {
     // Implement fetching purchased courses logic
+    const username = req.headers.username;
+    const user = await User.findOne({username})
+    console.log(user.purchasedCourses);
+    const courses = await Course.find({
+        _id:{
+            "$in":user.purchasedCourses
+        }
+    })
+    res.json({
+        courses
+    })
 });
 
 module.exports = router
